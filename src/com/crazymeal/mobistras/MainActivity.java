6be812f2e0ef4e355189b49ec290576df6c2c9de
KVN <kvndesign.mail@gmail.com>
2013-10-02 18:45:07 +0200

    jout de vérification de connection à internet
    On vérifie que l'on a accès à internet afin de pouvoir récupérer les
    données

--- src/com/crazymeal/mobistras/MainActivity.java
+++ src/com/crazymeal/mobistras/MainActivity.java
@@ -1,7 +1,11 @@
 package com.crazymeal.mobistras;
 
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.NetworkInfo.State;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -19,4 +23,16 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	public boolean isConnected(Activity activity) 
+    {
+      ConnectivityManager connectivityManager = (ConnectivityManager)  activity.getSystemService(Context.CONNECTIVITY_SERVICE);
+      NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+      if (networkInfo != null) 
+      {
+        State networkState = networkInfo.getState();
+        if (networkState.compareTo(State.CONNECTED) == 0) 
+          return true;
+      }
+      return false;
+    }
 }
