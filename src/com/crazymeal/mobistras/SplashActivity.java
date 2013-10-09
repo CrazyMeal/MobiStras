0e8dba74ce5ac7fd183f4f03869c75863e6d9ade
KVN <kvndesign.mail@gmail.com>
2013-10-09 16:34:29 +0200

    Animation de sortie splashscreen
    Sortie du splashscreen en fade out

--- src/com/crazymeal/mobistras/SplashActivity.java
+++ src/com/crazymeal/mobistras/SplashActivity.java
@@ -5,29 +5,47 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.view.animation.AnimationUtils;
+import android.widget.LinearLayout;
 
 public class SplashActivity extends Activity {
 	private static final int STOPSPLASH = 0;
-
-	/**
-	 * Default duration for the splash screen (milliseconds)
-	 */
 	private static final long SPLASHTIME = 5000;
 
-	/**
-	 * Handler to close this activity and to start automatically
-	 * {@link MainActivity} after <code>SPLASHTIME</code> seconds.
-	 */
 	private final transient Handler splashHandler = new Handler() {
 		@Override
-		public void handleMessage(Message msg) {
-			if (msg.what == STOPSPLASH) {
-				final Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-				startActivity(intent);
-				finish();
-			}
+		public void handleMessage(Message msg)
+		{
+		    if (msg.what == STOPSPLASH)
+		    {
+		        final Animation animation = AnimationUtils.loadAnimation(getBaseContext(),
+		                                                                 android.R.anim.fade_out);
+		        animation.setAnimationListener(new AnimationListener()
+		        {
+		            @Override public void onAnimationEnd(Animation animation)
+		            {
+		                ((LinearLayout)findViewById(R.id.splash)).setVisibility(View.INVISIBLE);
+		                final Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+		                startActivity(intent);
+		                finish();
+		            }
 
-			super.handleMessage(msg);
+		            @Override public void onAnimationRepeat(Animation animation)
+		            {
+		            }
+
+		            @Override public void onAnimationStart(Animation animation)
+		            {
+		            }
+		        });
+
+		        ((LinearLayout)findViewById(R.id.splash)).startAnimation(animation);
+		    }
+
+		    super.handleMessage(msg);
 		}
 	};
 
