ce58d295e4495f99173fc00323268b92f84d4d2c
KVN <kvndesign.mail@gmail.com>
2013-10-09 15:32:30 +0200

    Mise en place du système de service
    Lors du lancement de l'application, un service est crée. A voir si le
    service doit être démarré à l'allumage de l'appareil. (Utilité à
    l'allumage ?)

--- src/com/crazymeal/mobistras/MainActivity.java
+++ src/com/crazymeal/mobistras/MainActivity.java
@@ -1,28 +1,82 @@
 package com.crazymeal.mobistras;
 
+
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.NetworkInfo.State;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.Messenger;
 import android.app.Activity;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.SharedPreferences;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
 
+	private Messenger mService = null;
+	private boolean mBound;
+	private SharedPreferences myPrefs;
+	
+	
+	private ServiceConnection mConnection = new ServiceConnection() {
+		public void onServiceConnected(ComponentName className, IBinder service) {
+			setmService(new Messenger(service));
+			setmBound(true);
+		}
+
+		public void onServiceDisconnected(ComponentName className) {
+			setmService(null);
+			setmBound(false);
+		}
+	};
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		try {
+			startService(new Intent(MainActivity.this, MainService.class));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_main, menu);
+		this.getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
 
+	@Override
+	public void onStart() {
+		super.onStart();
+		bindService(new Intent(this, MainService.class), mConnection,
+				Context.BIND_AUTO_CREATE);
+
+		final String PREFS_NAME = "MyPrefsFile";
+		setMyPrefs(this.getSharedPreferences(PREFS_NAME, MODE_PRIVATE));
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		if (mBound) {
+			unbindService(mConnection);
+			mBound = false;
+		}
+	}
+	
+	@Override
+	public void onPause() {
+		super.onPause();
+	}
+	
 	public boolean isConnected(Activity activity) 
     {
       ConnectivityManager connectivityManager = (ConnectivityManager)  activity.getSystemService(Context.CONNECTIVITY_SERVICE);
@@ -35,4 +89,28 @@ public class MainActivity extends Activity {
       }
       return false;
     }
+
+	public Messenger getmService() {
+		return mService;
+	}
+
+	public void setmService(Messenger mService) {
+		this.mService = mService;
+	}
+
+	public boolean ismBound() {
+		return mBound;
+	}
+
+	public void setmBound(boolean mBound) {
+		this.mBound = mBound;
+	}
+
+	public SharedPreferences getMyPrefs() {
+		return myPrefs;
+	}
+
+	public void setMyPrefs(SharedPreferences myPrefs) {
+		this.myPrefs = myPrefs;
+	}
 }
---
+++ src/com/crazymeal/mobistras/MainService.java
@@ -0,0 +1,49 @@
+package com.crazymeal.mobistras;
+
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.widget.Toast;
+
+public class MainService extends Service{
+	final Messenger mMessenger = new Messenger(new IncomingHandler());
+	
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+		Toast.makeText(getApplicationContext(), "binding", Toast.LENGTH_SHORT).show();
+		return mMessenger.getBinder();
+	}
+
+	
+	
+	class IncomingHandler extends Handler {
+		@Override
+		public void handleMessage(Message msg) {
+			/*
+			MessageInformations infos = (MessageInformations) msg.obj;
+			
+			Calendar AlarmCal = Calendar.getInstance();
+			AlarmCal.setTimeInMillis(System.currentTimeMillis());
+			AlarmCal.set(Calendar.HOUR_OF_DAY, infos.getHour());
+			AlarmCal.set(Calendar.MINUTE, infos.getMinute());
+			AlarmCal.set(Calendar.SECOND, 0);
+			
+			am = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+			
+			Intent intent = new Intent(SenderService.this, TimeAlarm.class);
+			
+			intent.putExtra("destinator", infos.getDestinator());
+			intent.putExtra("messageToSend", infos.getMessageToSend());
+			
+			int random = (int)(Math.random() * 10) + 9;
+			PendingIntent pendingIntent = PendingIntent.getBroadcast(SenderService.this, random,intent,PendingIntent.FLAG_ONE_SHOT);
+			am.set(AlarmManager.RTC_WAKEUP,AlarmCal.getTimeInMillis(),pendingIntent);
+		*/
+		}
+	}
+}
