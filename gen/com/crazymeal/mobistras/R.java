75d1bb686c825bf96f60e25f1b753ca3152e57dd
KVN <kvndesign.mail@gmail.com>
2015-01-26 10:00:14 +0100

    Some clean

--- gen/com/crazymeal/mobistras/R.java
+++ gen/com/crazymeal/mobistras/R.java
@@ -19,55 +19,47 @@ public final class R {
         public static final int parking_open=0x7f020002;
     }
     public static final class id {
-        public static final int button_validate_alarm=0x7f08000a;
-        public static final int checkBox_isRecurrent=0x7f080002;
-        public static final int imageView_favorite=0x7f080010;
-        public static final int imageView_parking_logo=0x7f08000c;
-        public static final int layout_recurrence=0x7f080004;
-        public static final int layout_recurrence_panel=0x7f080003;
-        public static final int layout_time=0x7f080007;
-        public static final int listview_database=0x7f080000;
-        public static final int listviewperso=0x7f08000b;
-        public static final int menu_settings=0x7f080011;
-        public static final int spinner_recurrence=0x7f080006;
-        public static final int textView1=0x7f08000e;
-        public static final int textView2=0x7f080001;
-        public static final int textView_available_places=0x7f08000f;
-        public static final int textView_parking_name=0x7f08000d;
-        public static final int text_view_recurrence=0x7f080005;
-        public static final int text_view_time=0x7f080008;
-        public static final int timePicker=0x7f080009;
+        public static final int button_validate_alarm=0x7f080009;
+        public static final int checkBox_isRecurrent=0x7f080001;
+        public static final int imageView_favorite=0x7f08000f;
+        public static final int imageView_parking_logo=0x7f08000b;
+        public static final int layout_recurrence=0x7f080003;
+        public static final int layout_recurrence_panel=0x7f080002;
+        public static final int layout_time=0x7f080006;
+        public static final int listviewperso=0x7f08000a;
+        public static final int menu_settings=0x7f080010;
+        public static final int spinner_recurrence=0x7f080005;
+        public static final int textView1=0x7f08000d;
+        public static final int textView2=0x7f080000;
+        public static final int textView_available_places=0x7f08000e;
+        public static final int textView_parking_name=0x7f08000c;
+        public static final int text_view_recurrence=0x7f080004;
+        public static final int text_view_time=0x7f080007;
+        public static final int timePicker=0x7f080008;
     }
     public static final class layout {
-        public static final int activity_database=0x7f030000;
-        public static final int activity_parking_alarm=0x7f030001;
-        public static final int list=0x7f030002;
-        public static final int new_list_item_display=0x7f030003;
+        public static final int activity_parking_alarm=0x7f030000;
+        public static final int list=0x7f030001;
+        public static final int new_list_item_display=0x7f030002;
     }
     public static final class menu {
         public static final int activity_parking_list=0x7f070000;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
-        public static final int content_description_favorite=0x7f050012;
-        public static final int content_description_logo_map=0x7f05000e;
-        public static final int content_description_parking_logo=0x7f05000d;
-        public static final int description_image_parking_open=0x7f050003;
+        public static final int content_description_favorite=0x7f05000d;
+        public static final int content_description_logo_map=0x7f05000a;
+        public static final int content_description_parking_logo=0x7f050009;
+        public static final int description_image_parking_open=0x7f05000b;
         public static final int menu_settings=0x7f050001;
-        public static final int text_alarm_infos=0x7f050009;
-        public static final int text_available_places=0x7f05000a;
-        public static final int text_button_preferences=0x7f050002;
-        public static final int text_button_validate_alarm=0x7f050008;
-        public static final int text_detailed_alarms=0x7f050010;
-        public static final int text_detailed_isreccurent=0x7f050011;
-        public static final int text_full_places=0x7f05000b;
-        public static final int text_navigate_to=0x7f05000c;
-        public static final int text_programmed_alarms=0x7f05000f;
-        public static final int text_recurrence=0x7f050006;
-        public static final int text_time=0x7f050007;
-        public static final int title_activity_notification_landing=0x7f050013;
-        public static final int urlLocation=0x7f050004;
-        public static final int urlParking=0x7f050005;
+        public static final int text_alarm_infos=0x7f050007;
+        public static final int text_available_places=0x7f050008;
+        public static final int text_button_validate_alarm=0x7f050006;
+        public static final int text_detailed_isreccurent=0x7f05000c;
+        public static final int text_recurrence=0x7f050004;
+        public static final int text_time=0x7f050005;
+        public static final int urlLocation=0x7f050002;
+        public static final int urlParking=0x7f050003;
     }
     public static final class style {
         /** 
