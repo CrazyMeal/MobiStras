e2df6fa16120785fc1a69e9c5d3b1b5676df3bbc
KVN <kvndesign.mail@gmail.com>
2015-01-26 19:55:42 +0100

    Preventing when no internet access when alarm rings

--- src/com/crazymeal/strasandpark/alarms/AlarmReceiver.java
+++ src/com/crazymeal/strasandpark/alarms/AlarmReceiver.java
@@ -13,6 +13,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.IBinder;
 import android.os.Message;
 import android.os.Messenger;
@@ -77,52 +79,58 @@ public class AlarmReceiver extends BroadcastReceiver {
 	private void handleAlarm(Context context, Intent intent){
 		Log.d("SIMPLE_ALARM", "JE SONNE");
 		int day = intent.getIntExtra("day", -1);
+		
 		if(day != -1){
 			Calendar calendar = Calendar.getInstance();
 			int androidDay = calendar.get(Calendar.DAY_OF_WEEK);
+			
 			if(day == androidDay || day == 0){
 				Log.d("SIMPLE_ALARM", "Today we ring> " + String.valueOf(day));
+				StringBuffer sb = new StringBuffer();
 				
-				ParkingDatabase db = new ParkingDatabase(context);
-				HashMap<Integer, String> parkingIds = db.getAllFavorites();
-				db.close();
-				if(!parkingIds.isEmpty()){
-					JsonDownloadTask task = new JsonDownloadTask();
-					task.execute(new String[]{context.getString(R.string.urlParking)});
-					String taskResult = "";
-					try {
-						taskResult = task.get();	
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					} catch (ExecutionException e) {
-						e.printStackTrace();
-					}
-					StringBuffer sb = new StringBuffer();
-					
-					HashMap<Integer, Parking> parkingMap = new JsonParkingParser().parse(taskResult);
-					for(Entry<Integer, String> parkingEntry : parkingIds.entrySet()){
+				if(this.isOnline(context)){
+					ParkingDatabase db = new ParkingDatabase(context);
+					HashMap<Integer, String> parkingIds = db.getAllFavorites();
+					db.close();
+					if(!parkingIds.isEmpty()){
+						JsonDownloadTask task = new JsonDownloadTask();
+						task.execute(new String[]{context.getString(R.string.urlParking)});
+						String taskResult = "";
+						try {
+							taskResult = task.get();	
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						} catch (ExecutionException e) {
+							e.printStackTrace();
+						}
 						
-						if(parkingMap.containsKey(parkingEntry.getKey())){
-							Parking newParking = parkingMap.get(parkingEntry.getKey());
+						HashMap<Integer, Parking> parkingMap = new JsonParkingParser().parse(taskResult);
+						for(Entry<Integer, String> parkingEntry : parkingIds.entrySet()){
 							
-							Log.d("SIMPLE_ALARM", "parking with id "+ newParking.getId() + " has " + newParking.getAvaiblePlaces() + " places");
-							switch (newParking.getStatus()) {
-							case CLOSE:
-								sb.append(parkingEntry.getValue() + " est fermé");
-								break;
-							case FULL:
-								sb.append(parkingEntry.getValue() + " est complet");
-								break;
-							default:
-								sb.append(parkingEntry.getValue() + "> reste " + newParking.getAvaiblePlaces() + " places");
-								break;
+							if(parkingMap.containsKey(parkingEntry.getKey())){
+								Parking newParking = parkingMap.get(parkingEntry.getKey());
+								
+								Log.d("SIMPLE_ALARM", "parking with id "+ newParking.getId() + " has " + newParking.getAvaiblePlaces() + " places");
+								switch (newParking.getStatus()) {
+								case CLOSE:
+									sb.append(parkingEntry.getValue() + " est fermé");
+									break;
+								case FULL:
+									sb.append(parkingEntry.getValue() + " est complet");
+									break;
+								default:
+									sb.append(parkingEntry.getValue() + "> reste " + newParking.getAvaiblePlaces() + " places");
+									break;
+								}
+								sb.append(System.getProperty("line.separator"));
 							}
-							sb.append(System.getProperty("line.separator"));
 						}
 					}
-					this.buildNotification(sb.toString(), context);
+					
+				} else {
+					sb.append("Impossible de faire la vérification sans connection internet");
 				}
-				
+				this.buildNotification(sb.toString(), context);
 			} else
 				Log.d("SIMPLE_ALARM", "Not today");
 		}
@@ -148,5 +156,11 @@ public class AlarmReceiver extends BroadcastReceiver {
 
 		int random = (int)(Math.random() * 10) + 9;
 		mNotificationManager.notify(random, notif);
+	}
+	
+	public boolean isOnline(Context context) {
+	    ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+	    NetworkInfo netInfo = cm.getActiveNetworkInfo();
+	    return netInfo != null && netInfo.isConnectedOrConnecting();
 	}
 }
