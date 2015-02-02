636c9baaf2871b6b4fc7a1851b5347597f16f9e3
KVN <kvndesign.mail@gmail.com>
2015-02-02 19:40:00 +0100

    Adding pull to refresh

--- src/com/crazymeal/strasandpark/activities/ParkingListActivity.java
+++ src/com/crazymeal/strasandpark/activities/ParkingListActivity.java
@@ -16,11 +16,14 @@ import android.os.Message;
 import android.os.Messenger;
 import android.os.RemoteException;
 import android.os.ResultReceiver;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v4.widget.SwipeRefreshLayout.OnRefreshListener;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
+import android.widget.AbsListView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
@@ -35,6 +38,7 @@ public class ParkingListActivity extends Activity{
 	private ParkingMapAdapter adapter;
 	private ListView listView;
 	private LinearLayout internetLayout;
+	private SwipeRefreshLayout swipeLayout;
 	private Context thatContext;
 	
 	private Messenger serviceMessenger;
@@ -45,6 +49,41 @@ public class ParkingListActivity extends Activity{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.list);
 		this.thatContext = this;
+		
+		this.swipeLayout = (SwipeRefreshLayout) findViewById(R.id.swipe);
+		this.swipeLayout.setEnabled(false);
+		
+		this.listView = (ListView) findViewById(R.id.listviewperso);
+		this.listView = (ListView) findViewById(R.id.listviewperso);
+		this.listView.setOnScrollListener(new AbsListView.OnScrollListener() {
+			@Override
+			public void onScrollStateChanged(AbsListView view, int scrollState) {
+			}
+			@Override
+			public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+				if (firstVisibleItem == 0)
+					swipeLayout.setEnabled(true);
+				else
+					swipeLayout.setEnabled(false);
+			}
+		});
+		
+		
+		this.swipeLayout.setOnRefreshListener(new OnRefreshListener() {
+			@Override
+			public void onRefresh() {
+				swipeLayout.setRefreshing(true);
+				if(isOnline()){
+					try {
+						Message msg = Message.obtain();
+						msg.arg1 = 0;
+						serviceMessenger.send(msg);
+					} catch (RemoteException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		});
 		
 		this.mConnection = new ServiceConnection() {
 			public void onServiceConnected(ComponentName className, IBinder service) {
@@ -87,9 +126,6 @@ public class ParkingListActivity extends Activity{
 				serviceMessenger = null;
 			}
 		};
-		this.listView = (ListView) findViewById(R.id.listviewperso);
-		
-		
 	}
 	
 	@Override
@@ -143,6 +179,7 @@ public class ParkingListActivity extends Activity{
 				db.close();
 				listView.setAdapter(new ParkingMapAdapter(this.context, context.getResources(), R.layout.new_list_item_display, finalList));
 				listView.setVisibility(View.VISIBLE);
+				swipeLayout.setRefreshing(false);
 			}
 		}
 		
